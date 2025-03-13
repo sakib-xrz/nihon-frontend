4d367c9072e5d48754b0e907ab336f935ca25020
sakib-xrz <sakibxrz21@gmail.com>
2025-03-13 22:06:29 +0600

    improve sticyness

--- src/app/(dashboard)/dashboard/layout.jsx
+++ src/app/(dashboard)/dashboard/layout.jsx
@@ -1,39 +1,51 @@
 "use client";
-import AdminRouter from '@/components/AdminRouter/AdminRouter';
-import DashbrodNavbar from '@/components/DashbrodNavbar/DashbrodNavbar';
-import menuItems from '@/components/DashbrodNavbar/DashbrodNavbarMenu';
-import { Layout, Menu } from 'antd';
-import { Toaster } from 'sonner';
+import AdminRouter from "@/components/AdminRouter/AdminRouter";
+import DashbrodNavbar from "@/components/DashbrodNavbar/DashbrodNavbar";
+import menuItems from "@/components/DashbrodNavbar/DashbrodNavbarMenu";
+import { Layout, Menu } from "antd";
+import { Toaster } from "sonner";
 
 const { Header, Content, Sider } = Layout;
 
 const Page = ({ children }) => {
-
   return (
     <Layout>
       <Sider
-        style={{ background: 'white' }}
+        style={{ background: "white" }}
         breakpoint="lg"
         width="300px"
         collapsedWidth="0"
       >
-        <div className="demo-logo-vertical" />
-        <Menu theme="light" mode="inline" defaultSelectedKeys={['4']} items={menuItems} />
+        <div className="sticky top-0">
+          <Menu
+            theme="light"
+            mode="inline"
+            defaultSelectedKeys={["4"]}
+            items={menuItems}
+          />
+        </div>
       </Sider>
       <Layout>
-        <Header style={{ background: 'white' }}>
+        <Header
+          style={{
+            background: "white",
+            position: "sticky",
+            top: 0,
+            zIndex: 1000,
+          }}
+        >
           <DashbrodNavbar />
         </Header>
         <Content
           style={{
-            margin: '24px 16px 0',
+            margin: "24px 16px 0",
           }}
         >
           <div
             style={{
               padding: 24,
-              height: 'auto',
-              background: 'white',
+              height: "auto",
+              background: "white",
             }}
           >
             {children}
